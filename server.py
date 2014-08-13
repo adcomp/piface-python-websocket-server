c17b4b47fc3106b507b352588de28ef57fb5d590
David Art <david.madbox@gmail.com>
2014-08-13 16:53:37 +0200

    merge css/js into index.html, fix websocket and add HTTP server

--- server.py
+++ server.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python
 
-###
-#	Python WebSocket Server for Raspberry Pi
-#	PiFace web control
-#	by David Art <david.madbox@gmail.com>
-###
+# Python WebSocket Server for Raspberry Pi / PiFace
+# by David Art [aka] adcomp <david.madbox@gmail.com>
 
 import tornado.httpserver
 import tornado.websocket
@@ -13,63 +10,75 @@ import tornado.web
 
 import datetime
 import json
+import sys
+import os
 
-# import piface.pfio as pfio
+# PiFace module & init
 import pifacedigitalio as pfio
 pfio.init()
 pifacedigital = pfio.PiFaceDigital()
 
-last_data = None
+class IndexHandler(tornado.web.RequestHandler):
+    @tornado.web.asynchronous
+    def get(self):
+        #self.write("This is your response")
+        self.render("index.html")
 
-class WSHandler(tornado.websocket.WebSocketHandler):
+class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
-	def open(self):
-		self.connected = True
-		print 'new connection'
-		self.timeout_loop()
-	  
-	def on_message(self, message):
-		global last_data
-		# print '> %s' % message
-		pin = int(message)
-		state = int(last_data['out'][pin])
-		# invert (toggle)
-		state = not bool(state)
-		pfio.digital_write(pin, state)
- 
-	def on_close(self):
-		self.connected = False
-		print 'connection closed'
-	  
-	def timeout_loop(self):
-		# read PiFace input/output state
-		global last_data
-		r_input = '{0:08b}'.format(pifacedigital.input_port.value)
-		r_output = '{0:08b}'.format(pifacedigital.output_port.value)
+  clients = []
+  last_data = None
 
-		data = {"in": [], "out": []}
+  def open(self):
+    self.connected = True
+    print 'new connection'
+    self.clients.append(self)
+    self.timeout_loop()
 
-		for i in range(8):
-			data['in'].append(r_input[7-i])
-			data['out'].append(r_output[7-i])
+  def on_message(self, message):
+    pin = int(message)
+    r_output = '{0:08b}'.format(pifacedigital.output_port.value)
+    pin_state = int(r_output[7-pin]);
+    pfio.digital_write(pin, not pin_state)
+    self.timeout_loop()
 
-		if data != last_data:
-			self.write_message(json.dumps(data))
-		last_data = data
+  def on_close(self):
+    self.connected = False
+    print 'connection closed'
+    self.clients.remove(self)
 
-		# here come the magic part .. loop
-		if self.connected:
-			tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=.5), self.timeout_loop)
- 
- 
+  def timeout_loop(self):
+    # read PiFace input/output state
+    r_input = '{0:08b}'.format(pifacedigital.input_port.value)
+    r_output = '{0:08b}'.format(pifacedigital.output_port.value)
+
+    data = {"in": [], "out": []}
+
+    for i in range(8):
+      data['in'].append(r_input[7-i])
+      data['out'].append(r_output[7-i])
+
+    if data != self.last_data:
+      for client in self.clients:
+        client.write_message(json.dumps(data))
+    self.last_data = data
+
+    # here come the magic part .. loop
+    if self.connected:
+      tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=.5), self.timeout_loop)
+
 application = tornado.web.Application([
-	(r'/piface', WSHandler),
+  (r'/', IndexHandler),
+  (r'/piface', WebSocketHandler)
 ])
 
 if __name__ == "__main__":
-	http_server = tornado.httpserver.HTTPServer(application)
-	http_server.listen(8888)
-	print("Raspberry Pi - PiFace")
-	print("WebSocket Server start ..")
-	tornado.ioloop.IOLoop.instance().start()
-
+  http_server = tornado.httpserver.HTTPServer(application)
+  http_server.listen(8888)
+  print("Raspberry Pi - PiFace")
+  print("WebSocket Server start ..")
+  try:
+    tornado.ioloop.IOLoop.instance().start()
+  except KeyboardInterrupt:
+    print('\nExit ..')
+    sys.exit(0)
