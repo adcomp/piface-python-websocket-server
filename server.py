ce1dfc4c4840d608d89c0583344d2c6225cebc67
David Art <david.madbox@gmail.com>
2014-08-15 14:40:48 +0200

    fix "print" & add comment

--- server.py
+++ server.py
@@ -21,7 +21,6 @@ pifacedigital = pfio.PiFaceDigital()
 class IndexHandler(tornado.web.RequestHandler):
     @tornado.web.asynchronous
     def get(self):
-        #self.write("This is your response")
         self.render("index.html")
 
 class WebSocketHandler(tornado.websocket.WebSocketHandler):
@@ -31,20 +30,24 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
 
   def open(self):
     self.connected = True
-    print 'new connection'
+    print('new connection')
     self.clients.append(self)
     self.timeout_loop()
 
   def on_message(self, message):
+    # message is the PIN number you want to toggle
     pin = int(message)
+    # read output state
     r_output = '{0:08b}'.format(pifacedigital.output_port.value)
+    # toggle output
     pin_state = int(r_output[7-pin]);
     pfio.digital_write(pin, not pin_state)
+    # FIXME! I need to check with the loop so I don't send twice ?
     self.timeout_loop()
 
   def on_close(self):
     self.connected = False
-    print 'connection closed'
+    print('connection closed')
     self.clients.remove(self)
 
   def timeout_loop(self):
@@ -52,18 +55,22 @@ class WebSocketHandler(tornado.websocket.WebSocketHandler):
     r_input = '{0:08b}'.format(pifacedigital.input_port.value)
     r_output = '{0:08b}'.format(pifacedigital.output_port.value)
 
+    # obj -> javascript
     data = {"in": [], "out": []}
 
     for i in range(8):
       data['in'].append(r_input[7-i])
       data['out'].append(r_output[7-i])
 
+    # only send message if input/output changed
     if data != self.last_data:
       for client in self.clients:
         client.write_message(json.dumps(data))
     self.last_data = data
 
     # here come the magic part .. loop
+    # FIXME! this is going pretty bad if too many clients I think ..
+    # no other way to do this ?
     if self.connected:
       tornado.ioloop.IOLoop.instance().add_timeout(datetime.timedelta(seconds=.5), self.timeout_loop)
 
